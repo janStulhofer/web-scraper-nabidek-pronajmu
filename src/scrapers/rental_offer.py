efc0c47a5c23489cf5e3110fb63d589a14dc77ae
janStulhofer <148527823+janStulhofer@users.noreply.github.com>
2025-03-18 21:13:08 +0100

    Update rental_offer.py

--- src/scrapers/rental_offer.py
+++ src/scrapers/rental_offer.py
@@ -6,24 +6,13 @@ class RentalOffer:
     """Nabídka pronájmu bytu"""
 
     link: str
-    """URL adresa na nabídku"""
-
     title: str
-    """Popis nabídky (nejčastěji počet pokojů, výměra)"""
-
     location: str
-    """Lokace bytu (městská část, ulice)"""
-
     price: int | str
-    """Cena pronájmu za měsíc bez poplatků a energií"""
-
     image_url: str
-    """Náhledový obrázek nabídky"""
-
     scraper: 'ScraperBase'
-    """Odkaz na instanci srapera, ze kterého tato nabídka pochází"""
-    
-@property
+
+    @property  # Správně odsazeno (4 mezery/tab pod třídou)
     def unique_hash(self) -> str:
         """Unikátní identifikátor kombinující titul, cenu a lokaci"""
         unique_str = f"{self.title}{self.price}{self.location}"
