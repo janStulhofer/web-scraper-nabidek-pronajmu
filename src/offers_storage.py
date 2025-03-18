ce032739c981fffd64f9241d4ca387e3b175a541
janStulhofer <148527823+janStulhofer@users.noreply.github.com>
2025-03-18 20:59:57 +0100

    Update offers_storage.py

--- src/offers_storage.py
+++ src/offers_storage.py
@@ -8,6 +8,32 @@ class OffersStorage:
 
     def __init__(self, path: str):
         self.path = path
+        self.first_time = False
+        self._hashes: set[str] = set()  
+        try:
+            with open(self.path) as file:
+                for line in file:
+                    self._hashes.add(line.strip())
+        except FileNotFoundError:
+            self.first_time = True
+
+    def contains(self, offer: RentalOffer) -> bool:
+        # Generujeme hash z titulku, ceny a lokace
+        unique_str = f"{offer.title}{offer.price}{offer.location}"
+        offer_hash = hashlib.md5(unique_str.encode()).hexdigest()
+        return offer_hash in self._hashes
+
+    def save_offers(self, offers: list[RentalOffer]):
+        with open(self.path, 'a+') as file_object:
+            for offer in offers:
+                unique_str = f"{offer.title}{offer.price}{offer.location}"
+                offer_hash = hashlib.md5(unique_str.encode()).hexdigest()
+                self._hashes.add(offer_hash)
+                file_object.write(offer_hash + os.linesep)
+
+        self.first_time = False
+
+        
         """Cesta k uloženým odkazům"""
 
         self.first_time = False
